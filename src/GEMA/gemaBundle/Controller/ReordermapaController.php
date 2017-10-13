e06e784bf84ecfa231dd35885898df2250210902
hl <zeldax54@gmail.com>
2017-10-13 18:15:41 -0400

    htacces code

---
+++ src/GEMA/gemaBundle/Controller/ReordermapaController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GEMA\gemaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ReordermapaController extends Controller
+{
+}
---
+++ src/GEMA/gemaBundle/Tests/Controller/ReordermapaControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GEMA\gemaBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ReordermapaControllerTest extends WebTestCase
+{
+}
